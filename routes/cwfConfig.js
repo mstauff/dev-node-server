a83262980dcd14353d2aacd87bcb4c1acde728ea
Matt Stauffer <matt.stauffer@ldschurch.org>
2016-08-17 12:49:18 -0600

    Add config json endpoint

---
+++ routes/cwfConfig.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send({
+        statuses: ['proposed', 'approved', 'accepted', 'declined', 'sustained', 'setApart'],
+        ldsEndpointUrls: [{MEMBER_LIST: '/directory/list'}, {CALLING_LIST: '/directory/callings'}],
+        orgTypes: [{id: 5, name: 'Bishopric'}, {id: 7, name: 'High Priests Group'}, {id: 9, name:'Primary'}]
+    });
+});
+
+module.exports = router;
