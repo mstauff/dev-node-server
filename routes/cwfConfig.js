937672d1f02b3aaffc31c00f1a21790d7c92f934
Matt Stauffer <matt.stauffer@ldschurch.org>
2016-08-17 13:41:53 -0600

    Adding express default files

--- routes/cwfConfig.js
+++ routes/cwfConfig.js
@@ -6,7 +6,7 @@ router.get('/', function (req, res, next) {
     res.send({
         statuses: ['proposed', 'approved', 'accepted', 'declined', 'sustained', 'setApart'],
         ldsEndpointUrls: [{MEMBER_LIST: '/directory/list'}, {CALLING_LIST: '/directory/callings'}],
-        orgTypes: [{id: 5, name: 'Bishopric'}, {id: 7, name: 'High Priests Group'}, {id: 9, name:'Primary'}]
+        orgTypes: [{id: 5, name: 'Bishopric'}, {id: 7, name: 'High Priests Group'}, {id: 9, name:'Primary'}, {id: 11, name:'Relief Society'}]
     });
 });
 
