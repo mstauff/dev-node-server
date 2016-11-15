ebb9dc51d8bf5c35f22df7e57d0a24d235384709
mstauff <matt.stauffer@ldschurch.org>
2016-11-15 14:49:11 -0700

    updated query param references Fixed URL's for test directory (mobiledirectory is under a different URL in test, so we'll just go straight to /directory, should be same results)

--- routes/cwfConfig.js
+++ routes/cwfConfig.js
@@ -3,16 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    var environmentParm = req.param('env');
+    var environmentParm = req.query.env;
     var isTestEnv = environmentParm && environmentParm.toLowerCase() == 'test';
     var baseUrl = isTestEnv ? 'https://test.lds.org/' : 'https://www.lds.org/';
     var baseSigninUrl = isTestEnv ? 'https://signin-int.lds.org/' : 'https://signin.lds.org/';
+    var baseDirectoryInstance = isTestEnv ? 'directory/' : 'mobiledirectory/';
     res.send({
         statuses: ['proposed', 'approved', 'accepted', 'declined', 'sustained', 'setApart'],
-        ldsEndpointUrls: [{USER_DATA: baseUrl + 'mobiledirectory/services/v2/ldstools/current-user-detail'},
+        ldsEndpointUrls: [{USER_DATA: baseUrl + baseDirectoryInstance + 'services/v2/ldstools/current-user-detail'},
             {SIGN_IN: baseSigninUrl + 'login.html'},
             {SIGN_OUT: baseUrl + 'signinout/?lang=eng&signmeout'},
-            {MEMBER_LIST: baseUrl + 'mobiledirectory/services/ludrs/1.1/mem/mobile/member-detaillist/:unitNum'},
+            {MEMBER_LIST: baseUrl + baseDirectoryInstance + 'services/ludrs/1.1/mem/mobile/member-detaillist/:unitNum'},
             {CALLING_LIST: baseUrl + 'mls/mbr/services/orgs/sub-orgs-with-callings'},
         ],
         orgTypes: [{id: 1179, name: 'Bishopric'}, {id: 69, name: 'High Priests Group'}, {id: 70, name:'Elders Quorum'},
