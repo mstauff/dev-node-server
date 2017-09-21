d2d7d96d4d8cbd6ca7b06dca1adcf5c567d4e5fe
Matt Stauffer <matt.stauffer@ldschurch.org>
2017-09-21 10:44:55 -0600

    Deploy

--- routes/cwfConfig.js
+++ routes/cwfConfig.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    var environmentParm = req.query.env;
-    var isTestEnv = environmentParm && environmentParm.toLowerCase() == 'test';
-    var baseUrl = isTestEnv ? 'https://test.lds.org/' : 'https://www.lds.org/';
+    var environmentParm = req.query.env && req.query.env.toLowerCase();
+    var isTestEnv = environmentParm && (environmentParm === 'test' || environmentParm === 'dev' || environmentParm === 'stage');
+    var baseUrl = isTestEnv ? 'https://' + environmentParm + '.lds.org/' : 'https://www.lds.org/';
     var baseSigninUrl = isTestEnv ? 'https://signin-int.lds.org/' : 'https://signin.lds.org/';
     var baseDirectoryInstance = isTestEnv ? 'directory/' : 'mobiledirectory/';
     res.send({
