afea62e1372f0666cdec9d9ec6bdabfc09432c33
Matt Stauffer <matt.stauffer@ldschurch.org>
2018-08-07 13:44:43 -0600

    Update lcr urls that have changed from lds.org/mls/mbr to lcr.lds.org

--- routes/cwfConfig.js
+++ routes/cwfConfig.js
@@ -7,6 +7,7 @@ router.get('/', function (req, res, next) {
     var signinParm = req.query.signIn && req.query.signIn.toLowerCase();
     var isTestEnv = environmentParm && (environmentParm === 'test' || environmentParm === 'dev' || environmentParm === 'stage');
     var baseUrl = isTestEnv ? 'https://' + environmentParm + '.lds.org/' : 'https://www.lds.org/';
+    var baseLcrUrl = isTestEnv ? 'https://lcr-' + environmentParm + '.lds.org/' : 'https://lcr.lds.org/';
     var baseSigninUrl = isTestEnv ? 'https://signin-int.lds.org/' : 'https://signin.lds.org/';
     // we've some switches in dev between signin-int.lds.org and ident-int.lds.org, seemingly  randomly. So we're adding
     // it as an optional parameter so we don't have to deploy a new version of the app with a change to the baseSigninUrl
@@ -20,14 +21,14 @@ router.get('/', function (req, res, next) {
             SIGN_IN: baseSigninUrl + 'login.html',
             SIGN_OUT: baseUrl + 'signinout/?lang=eng&signmeout',
             MEMBER_LIST: baseUrl + 'htvt/services/v1/:unitNum/members',
-            INDIVIDUAL_MEMBERS_LIST: baseUrl + 'mls/mbr//services/umlu/report/member-list?unitNumber=:unitNum',
+            INDIVIDUAL_MEMBERS_LIST: baseLcrUrl + 'services/umlu/report/member-list?unitNumber=:unitNum',
 
             MEMBER_LIST_SECONDARY: baseUrl + baseDirectoryInstance + 'services/ludrs/1.1/mem/mobile/member-detaillist/:unitNum',
             // MEMBER_LIST: baseUrl + baseDirectoryInstance + 'services/v2/ldstools/member-detaillist-with-callings/56030',
-            UPDATE_CALLING: baseUrl + 'mls/mbr/services/orgs/callings?lang=eng',
-            CALLING_LIST: baseUrl + 'mls/mbr/services/orgs/sub-orgs-with-callings',
-            UNIT_CALLINGS: baseUrl + 'mls/mbr/services/report/members-with-callings?unitNumber=:unitNum',
-            CLASS_ASSIGNMENTS: baseUrl + 'mls/mbr/services/orgs/sub-orgs-with-callings/?subOrgId=:subOrgId'
+            UPDATE_CALLING: baseLcrUrl + 'services/orgs/callings?lang=eng',
+            CALLING_LIST: baseLcrUrl + 'services/orgs/sub-orgs-with-callings',
+            UNIT_CALLINGS: baseLcrUrl + 'services/report/members-with-callings?unitNumber=:unitNum',
+            CLASS_ASSIGNMENTS: baseLcrUrl + 'services/orgs/sub-orgs-with-callings/?subOrgId=:subOrgId'
         },
         orgTypes: [{id: 1179, name: 'Bishopric'},  {id: 70, name:'Elders Quorum'},
             {id: 74, name:'Relief Society'}, {id: 73, name:'Young Men'}, {id: 76, name:'Young Women'},
