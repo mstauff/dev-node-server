3989f37fcced61d913432e13b8e536cda57dffd7
Matt Stauffer <matt.stauffer@ldschurch.org>
2019-05-30 12:40:39 -0600

    add params for app status

--- routes/cwfConfig.js
+++ routes/cwfConfig.js
@@ -9,6 +9,25 @@ router.get('/', function (req, res, next) {
     var baseUrl = isTestEnv ? 'https://' + environmentParm + '.churchofjesuschrist.org/' : 'https://www.lds.org/';
     var baseLcrUrl = isTestEnv ? 'https://lcr-' + environmentParm + '.lds.org/' : 'https://lcr.lds.org/';
     var baseSigninUrl = isTestEnv ? 'https://signin-int.lds.org/' : 'https://signin.lds.org/';
+
+    let STATUS_OK = 'OK';
+    let STATUS_WARN = 'WARN';
+    let STATUS_ERROR = 'ERROR';
+
+    // if we need to warn the user that something isn't working (most likely due to changes at the church), we'll do that
+    // by changing appStatus to either STATUS_WARN or STATUS_ERROR, then the app will provide the appStatusMsg to the user
+    var appStatus = STATUS_OK;
+    var appStatusMsg = '';
+
+    if( isTestEnv ) {
+        var status = req.query.status && req.query.status.toUpperCase()
+        if( status ) {
+            appStatus = status;
+            appStatusMsg = 'This is a ' + status + ' message.';
+        }
+    }
+
+
     // we've some switches in dev between signin-int.lds.org and ident-int.lds.org, seemingly  randomly. So we're adding
     // it as an optional parameter so we don't have to deploy a new version of the app with a change to the baseSigninUrl
     // everytime the church changes something.
@@ -18,6 +37,7 @@ router.get('/', function (req, res, next) {
     var baseDirectoryInstance = isTestEnv ? 'directory/' : 'mobiledirectory/';
     res.send({
         ldsEndpointUrls: {USER_DATA: baseUrl + baseDirectoryInstance + 'services/v2/ldstools/current-user-detail',
+        // ldsEndpointUrls: {USER_DATA: 'https://www.churchofjesuschrist.org/mobiledirectory/services/v2/ldstools/current-user-detail',
             SIGN_IN: baseSigninUrl + 'login.html',
             SIGN_OUT: baseUrl + 'signinout/?lang=eng&signmeout',
             MEMBER_LIST: baseUrl + 'htvt/services/v1/:unitNum/members',
@@ -30,10 +50,15 @@ router.get('/', function (req, res, next) {
             UNIT_CALLINGS: baseLcrUrl + 'services/report/members-with-callings?unitNumber=:unitNum',
             CLASS_ASSIGNMENTS: baseLcrUrl + 'services/orgs/sub-orgs-with-callings/?subOrgId=:subOrgId'
         },
-        // ldsBasicAuth: [],
+        ldsBasicAuth: [],
         ldsBasicAuth: ['USER_DATA'],
-        ldsHeaders: {},
-        // ldsHeaders: {USER_DATA : {lds_api_key : '150502025'}},
+        // ldsHeaders: {},
+        ldsHeaders: {USER_DATA : {lds_api_key : '10544b7b-2661-467b-a271-82338cf4a8aa',
+            'User-Agent': 'LDS Tools/3.6.1 (org.lds.ldstools; build:6526; iOS 12.2.0) Alamofire/4.8.1'}},
+        appStatus: {
+            status: appStatus,
+            message: appStatusMsg
+        },
         orgTypes: [{id: 1179, name: 'Bishopric'},  {id: 70, name:'Elders Quorum'},
             {id: 74, name:'Relief Society'}, {id: 73, name:'Young Men'}, {id: 76, name:'Young Women'},
             {id: 75, name:'Sunday School'},{id: 77, name:'Primary'},{id: 1310, name:'Ward Missionaries'},
